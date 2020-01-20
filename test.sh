8bbda2c8f2453ce16cc0db32ce0e867007bb58b5
Bing1996 <pzhjsb@gmail.com>
2020-01-20 15:24:30 -0500

    inputdata

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 clear
 
 echo "installing SSR..."
+sudo clone https://raw.githubusercontent.com/Bing1996/LittleAboutLinux/master/input.data
 sudo git clone -b master https://github.com/flyzy2005/ss-fly
-sudo ss-fly/ss-fly.sh -ssr
+sudo ss-fly/ss-fly.sh -ssr < input.data
