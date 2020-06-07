774e4195ba380d8a02cef703c8b53c3107f58f66
Bing <38998599+Bing1996@users.noreply.github.com>
2020-06-07 19:56:16 -0400

    link

---
+++ link.py
@@ -0,0 +1,5 @@
+https://www.youtube.com/watch?v=tamPSHpac0A&t=10766s
+
+https://www.youtube.com/watch?v=3Ljgb8644fM&t=1130s
+
+https://s3.amazonaws.com/gnews-media-offload/wp-content/uploads/2020/06/03195730/%E3%80%90%E4%B8%AD%E6%96%87%E3%80%91%E6%96%B0%E4%B8%AD%E5%9B%BD%E8%81%94%E9%82%A6%E5%AE%A3%E8%A8%80.pdf
